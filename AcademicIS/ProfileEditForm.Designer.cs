88ed902ca9a05862fa81971479d0e626a8137d02
Bekir Uzun <bekiruzun8000@gmail.com>
2017-12-14 00:16:58 +0300

    Added initial profile add/edit form

---
+++ AcademicIS/ProfileEditForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace AcademicIS {
+    partial class ProfileEditForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTB = new FlatUI.FlatTextBox();
+            this.mailTB = new FlatUI.FlatTextBox();
+            this.phoneTB = new FlatUI.FlatTextBox();
+            this.webpageTB = new FlatUI.FlatTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.facultyCB = new FlatUI.FlatComboBox();
+            this.departmentCB = new FlatUI.FlatComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.detailRichTB = new System.Windows.Forms.RichTextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "E-posta:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(556, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon:";
+            // 
+            // nameTB
+            // 
+            this.nameTB.BackColor = System.Drawing.Color.Transparent;
+            this.nameTB.FocusOnHover = false;
+            this.nameTB.Location = new System.Drawing.Point(104, 24);
+            this.nameTB.MaxLength = 32767;
+            this.nameTB.Multiline = false;
+            this.nameTB.Name = "nameTB";
+            this.nameTB.ReadOnly = false;
+            this.nameTB.Size = new System.Drawing.Size(152, 29);
+            this.nameTB.TabIndex = 3;
+            this.nameTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.nameTB.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.nameTB.UseSystemPasswordChar = false;
+            // 
+            // mailTB
+            // 
+            this.mailTB.BackColor = System.Drawing.Color.Transparent;
+            this.mailTB.FocusOnHover = false;
+            this.mailTB.Location = new System.Drawing.Point(362, 24);
+            this.mailTB.MaxLength = 32767;
+            this.mailTB.Multiline = false;
+            this.mailTB.Name = "mailTB";
+            this.mailTB.ReadOnly = false;
+            this.mailTB.Size = new System.Drawing.Size(152, 29);
+            this.mailTB.TabIndex = 4;
+            this.mailTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.mailTB.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.mailTB.UseSystemPasswordChar = false;
+            // 
+            // phoneTB
+            // 
+            this.phoneTB.BackColor = System.Drawing.Color.Transparent;
+            this.phoneTB.FocusOnHover = false;
+            this.phoneTB.Location = new System.Drawing.Point(615, 24);
+            this.phoneTB.MaxLength = 32767;
+            this.phoneTB.Multiline = false;
+            this.phoneTB.Name = "phoneTB";
+            this.phoneTB.ReadOnly = false;
+            this.phoneTB.Size = new System.Drawing.Size(152, 29);
+            this.phoneTB.TabIndex = 5;
+            this.phoneTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.phoneTB.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.phoneTB.UseSystemPasswordChar = false;
+            // 
+            // webpageTB
+            // 
+            this.webpageTB.BackColor = System.Drawing.Color.Transparent;
+            this.webpageTB.FocusOnHover = false;
+            this.webpageTB.Location = new System.Drawing.Point(104, 74);
+            this.webpageTB.MaxLength = 32767;
+            this.webpageTB.Multiline = false;
+            this.webpageTB.Name = "webpageTB";
+            this.webpageTB.ReadOnly = false;
+            this.webpageTB.Size = new System.Drawing.Size(152, 29);
+            this.webpageTB.TabIndex = 7;
+            this.webpageTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.webpageTB.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.webpageTB.UseSystemPasswordChar = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Website:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(300, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Fakülte:";
+            // 
+            // facultyCB
+            // 
+            this.facultyCB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.facultyCB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.facultyCB.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.facultyCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.facultyCB.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.facultyCB.ForeColor = System.Drawing.Color.White;
+            this.facultyCB.FormattingEnabled = true;
+            this.facultyCB.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.facultyCB.ItemHeight = 18;
+            this.facultyCB.Location = new System.Drawing.Point(362, 76);
+            this.facultyCB.Name = "facultyCB";
+            this.facultyCB.Size = new System.Drawing.Size(152, 24);
+            this.facultyCB.TabIndex = 9;
+            // 
+            // departmentCB
+            // 
+            this.departmentCB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.departmentCB.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.departmentCB.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.departmentCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.departmentCB.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.departmentCB.ForeColor = System.Drawing.Color.White;
+            this.departmentCB.FormattingEnabled = true;
+            this.departmentCB.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.departmentCB.ItemHeight = 18;
+            this.departmentCB.Location = new System.Drawing.Point(615, 76);
+            this.departmentCB.Name = "departmentCB";
+            this.departmentCB.Size = new System.Drawing.Size(152, 24);
+            this.departmentCB.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(553, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Bölüm:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Detaylı Özgeçmiş:";
+            // 
+            // detailRichTB
+            // 
+            this.detailRichTB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+            this.detailRichTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.detailRichTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.detailRichTB.ForeColor = System.Drawing.Color.White;
+            this.detailRichTB.Location = new System.Drawing.Point(30, 153);
+            this.detailRichTB.Name = "detailRichTB";
+            this.detailRichTB.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.detailRichTB.Size = new System.Drawing.Size(737, 201);
+            this.detailRichTB.TabIndex = 13;
+            this.detailRichTB.Text = "";
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.saveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveButton.FlatAppearance.BorderSize = 0;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.saveButton.Location = new System.Drawing.Point(661, 370);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(0);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(106, 30);
+            this.saveButton.TabIndex = 14;
+            this.saveButton.Text = "Kaydet";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(85)))), ((int)(((byte)(96)))));
+            this.deleteButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.deleteButton.FlatAppearance.BorderSize = 0;
+            this.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteButton.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.deleteButton.Location = new System.Drawing.Point(45, 370);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(0);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(106, 30);
+            this.deleteButton.TabIndex = 15;
+            this.deleteButton.Text = "Sil";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.button1.Location = new System.Drawing.Point(346, 370);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 30);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "İptal";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // ProfileEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(795, 425);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.detailRichTB);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.departmentCB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.facultyCB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.webpageTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.phoneTB);
+            this.Controls.Add(this.mailTB);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ProfileEditForm";
+            this.Text = "ProfileEditForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private FlatUI.FlatTextBox nameTB;
+        private FlatUI.FlatTextBox mailTB;
+        private FlatUI.FlatTextBox phoneTB;
+        private FlatUI.FlatTextBox webpageTB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private FlatUI.FlatComboBox facultyCB;
+        private FlatUI.FlatComboBox departmentCB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox detailRichTB;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
