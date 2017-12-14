129750ab0a2a4273412476bc424790740761caa1
Berat Gumus <beratgumus9@gmail.com>
2017-12-14 22:49:44 +0300

    Implemented DB connection

---
+++ AcademicIS/DbHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcademicIS
+{
+    public class DbHelper
+    {
+        SqlConnection con;
+        public DbHelper()
+        {
+             con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\database.mdf;Integrated Security=True;Connect Timeout=30");
+        }
+
+        public DataTable getAcademicians()
+        {
+            DataTable table = new DataTable();
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Academician", con);
+            con.Open();
+            adp.Fill(table);
+            return table;
+        }
+    }
+}
