6b073e5ab2be4207ade59fdb1dca850106dbe083
Berat Gumus <beratgumus9@gmail.com>
2017-12-23 16:33:53 +0300

    New test added

---
+++ AcademicISTests/CourseScheduleFormTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AcademicIS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicIS.Tests
+{
+    [TestClass()]
+    public class CourseScheduleFormTests
+    {
+        [TestMethod()]
+        public void GetAcademicianIdTest()
+        {
+            int expectedId =1 ;
+            CourseScheduleForm CSF = new CourseScheduleForm(expectedId, false);
+            int actualId = CSF.GetAcademicianId();
+            Assert.AreEqual(expectedId, actualId);
+            bool editVisibility = CSF.GetEditButtonVisibility();
+            bool deleteVisibility = CSF.GetDeleteButtonVisibility();
+            Assert.AreEqual(false, editVisibility);
+            Assert.AreEqual(false, deleteVisibility);
+        }
+    }
+}
