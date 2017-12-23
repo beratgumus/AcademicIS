b6b57a61f8658ee376969f3cd95e4ef33dbd23d0
Bekir Uzun <bekiruzun8000@gmail.com>
2017-12-23 15:55:38 +0300

    Added FacDep class
    This simple class is added for handling faculty and department names and ids

---
+++ AcademicIS/FacDep.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicIS {
+    public class FacDep {
+        public int id;
+        public string name;
+
+        public FacDep(int id, string name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public FacDep() { }
+
+        public override string ToString() {
+            return name;
+        }
+
+        public override int GetHashCode() {
+            if (name == null)
+                return 0;
+
+            return name.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (obj == null)
+                return false;
+            
+            if (obj.GetType().Name == "FacDep") {
+                FacDep o = (FacDep)obj;
+                return o.id == this.id && o.name == this.name;
+            }
+            else
+                return false;
+        }
+    }
+}
