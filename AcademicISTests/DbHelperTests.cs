6e4e1d7f2a082cced5601c204e3600b6ea8ba017
Berat Gumus <beratgumus9@gmail.com>
2017-12-24 13:35:21 +0300

    Academician insertion test is done

---
+++ AcademicISTests/DbHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AcademicIS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicIS.Tests
+{
+    [TestClass()]
+    public class DbHelperTests
+    {
+        [TestMethod()]
+        public void GetAcademicianTest()
+        {
+            Academician ac = new Academician(-1,"Ahmet",1,"Mühendislik Fakültesi",4,"Elektrik Mühendisliği",
+                                              "ahmet@mail.com","5326569894","www.ahmet.com","Özgeçmiş burada :)");
+            DbHelper db = new DbHelper();
+            int insertedId = db.InsertAcademician(ac);
+            ac.Id = insertedId;
+            Academician acFromDb = db.GetAcademician(insertedId);
+
+            Assert.AreEqual(ac.Id, acFromDb.Id);
+            Assert.AreEqual(ac.Name, acFromDb.Name);
+            Assert.AreEqual(ac.Phone, acFromDb.Phone);
+        }
+    }
+}
