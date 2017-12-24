c82e85e31c45804d3b0817efa0690dbae23bd301
Berat Gumus <beratgumus9@gmail.com>
2017-12-24 21:22:33 +0300

    Test class is added for FacDep

---
+++ AcademicISTests/FacDepTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AcademicIS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicIS.Tests
+{
+    [TestClass()]
+    public class FacDepTests
+    {
+        [TestMethod()]
+        public void FacDepTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FacDepTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
